cf8a44842db9bfc529ecdffb4eed177ab05e2305
lee <lee@cppstyle.com>
2014-10-09 18:05:10 +0800

    adad

--- main.cpp
+++ main.cpp
@@ -1,19 +1,35 @@
-#include<iostream>  
+#include <iostream>  
+#include <vector>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>  
 #include <opencv2/highgui/highgui.hpp>  
 
-
+using namespace std;
 using namespace cv;
-
 
 int main()
 {
 	// 读入一张图片（游戏原画）  
-	Mat img = imread("e:/pp.jpg");
-	// 创建一个名为 "游戏原画"窗口  
-	namedWindow("游戏原画");
-	// 在窗口中显示游戏原画  
-	imshow("游戏原画", img);
-	// 等待6000 ms后窗口自动关闭  
-	waitKey(6000);
+	Mat src = imread("e:/pp.jpg");
+	vector<int> compression_params;
+	compression_params.push_back(95);
+	int i, j;
+	int cPointR, cPointG, cPointB, cPoint;//currentPoint;  
+	for (i = 1; i<src.rows; i++)
+	for (j = 1; j<src.cols; j++)
+	{
+		cPointB = src.at<Vec3b>(i, j)[0];
+		cPointG = src.at<Vec3b>(i, j)[1];
+		cPointR = src.at<Vec3b>(i, j)[2];
+		//if (cPointB>100 & cPointR<100 & cPointG<100)
+		{
+			src.at<Vec3b>(i, j)[0] = 0;
+			src.at<Vec3b>(i, j)[1] = 0;
+			src.at<Vec3b>(i, j)[2] = 0;
+		}
+
+	}
+	imwrite("e:/qq.jpg", src, compression_params);
+
+	return 0;
 }
